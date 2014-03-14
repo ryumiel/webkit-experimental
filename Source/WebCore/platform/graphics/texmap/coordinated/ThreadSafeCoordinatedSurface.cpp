8206329e9f04c3215feb8779109b254181c69757
Gwang Yoon Hwang <yoon@igalia.com>
2014-03-14 22:55:57 +0900

    [CoordGfx] Implement CoordinatedSurface for Threaded Coordinated Graphics https://bugs.webkit.org/show_bug.cgi?id=109661
    Patch by Jae Hyun Park <jae.park@company100.net> on 2013-07-02
    Reviewed by Noam Rosenthal.
    
    This is a preparation patch for Threaded Coordianted Graphics.
    
    This patch implements ThreadSafeCoordinatedSurface using ImageBuffer as a
    backend. Also, this patch moves common implementation to CoordinatedSurface to
    prevent duplication of code.

---
+++ Source/WebCore/platform/graphics/texmap/coordinated/ThreadSafeCoordinatedSurface.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2013 Company 100, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "ThreadSafeCoordinatedSurface.h"
+
+#if USE(COORDINATED_GRAPHICS)
+
+#if USE(TEXTURE_MAPPER)
+#include "TextureMapperGL.h"
+#endif
+
+namespace WebCore {
+
+PassRefPtr<ThreadSafeCoordinatedSurface> ThreadSafeCoordinatedSurface::create(const IntSize& size, CoordinatedSurface::Flags flags)
+{
+    return adoptRef(new ThreadSafeCoordinatedSurface(size, flags, ImageBuffer::create(size)));
+}
+
+PassRefPtr<ThreadSafeCoordinatedSurface> ThreadSafeCoordinatedSurface::create(const IntSize& size, CoordinatedSurface::Flags flags, std::unique_ptr<ImageBuffer> buffer)
+{
+    return adoptRef(new ThreadSafeCoordinatedSurface(size, flags, buffer));
+}
+
+ThreadSafeCoordinatedSurface::ThreadSafeCoordinatedSurface(const IntSize& size, CoordinatedSurface::Flags flags, std::unique_ptr<ImageBuffer> buffer)
+    : CoordinatedSurface(size, flags)
+    , m_imageBuffer(buffer)
+{
+}
+
+ThreadSafeCoordinatedSurface::~ThreadSafeCoordinatedSurface()
+{
+}
+
+void ThreadSafeCoordinatedSurface::paintToSurface(const IntRect& rect, CoordinatedSurface::Client* client)
+{
+    ASSERT(client);
+
+    GraphicsContext* context = beginPaint(rect);
+    client->paintToSurfaceContext(context);
+    endPaint();
+}
+
+GraphicsContext* ThreadSafeCoordinatedSurface::beginPaint(const IntRect& rect)
+{
+    ASSERT(m_imageBuffer);
+#if ENABLE(VD_CREATE_GRAPHICSCONTEXT)
+    GraphicsContext* graphicsContext = m_imageBuffer->createGraphicsContext();
+#else
+    GraphicsContext* graphicsContext = m_imageBuffer->context();
+#endif
+    graphicsContext->save();
+    graphicsContext->clip(rect);
+    graphicsContext->translate(rect.x(), rect.y());
+    return graphicsContext;
+}
+
+void ThreadSafeCoordinatedSurface::endPaint()
+{
+    ASSERT(m_imageBuffer);
+    m_imageBuffer->context()->restore();
+}
+
+#if USE(TEXTURE_MAPPER)
+void ThreadSafeCoordinatedSurface::copyToTexture(PassRefPtr<BitmapTexture> passTexture, const IntRect& target, const IntPoint& sourceOffset)
+{
+    RefPtr<BitmapTexture> texture(passTexture);
+
+    ASSERT(m_imageBuffer);
+    RefPtr<Image> image = m_imageBuffer->copyImage(DontCopyBackingStore);
+    texture->updateContents(image.get(), target, sourceOffset, BitmapTexture::UpdateCanModifyOriginalImageData);
+}
+#endif // USE(TEXTURE_MAPPER)
+
+} // namespace WebCore
+
+#endif // USE(COORDINATED_GRAPHICS)
